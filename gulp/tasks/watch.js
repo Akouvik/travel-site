3e71939f75a68e5f5ad9a2d2fd16df9793a33e6d
Akouvi <kouedjinl@gmail.com>
2016-10-06 15:56:16 -0700

    Completed the desktop and mobile header section

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -22,6 +22,9 @@ gulp.task('watch', function(){
   watch('./app/assets/styles/**/*.css', function(){
       gulp.start('cssInject');
     });
+    watch('./app/assets/scripts/**/*.js', function () {
+      gulp.start('scriptsRefresh');
+    })
   });
 
 
@@ -29,4 +32,9 @@ gulp.task('watch', function(){
   gulp.task('cssInject',['styles'], function(){
     return gulp.src('./app/temp/styles/styles.css')
         .pipe(browserSync.stream());
+  });
+
+
+  gulp.task('scriptsRefresh',['scripts'], function(){
+    browserSync.reload();
   });
